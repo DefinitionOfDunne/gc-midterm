ff8cf5330e0a07d49fa451d9969adec4972c0be7
Laura Senko <laurasenko@gmail.com>
2017-07-26 19:05:38 -0400

    .push changes

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
     console.log( "ready!" );
 
 var reservations= [];
-var seatReservation= "";
+var $seatReservation= "";
 
 $(".seat").on('click', function() {
 	$("#form").show();
-	seatReservation = $(this).attr("id");
-  console.log(seatReservation);
+	$seatReservation = $(this).attr("id");
+  reservations.push({seat: $seatReservation});
+  //console.log($seatReservation);
 });
 
 $("#submit").on('click', function() {
@@ -16,8 +17,17 @@ $("#submit").on('click', function() {
   var $email = $('#email').val();
   var $movie = $('#movie').val();
   var $time = $('#time').val();
-  console.log($name + $phone + $email + $movie + $time);
-});
+  //console.log($name + $phone + $email + $movie + $time);
+  reservations.push(
+    {name: $name},
+    {phone: $phone},
+    {email: $email},
+    {movie: $movie},
+    {time: $time}
+  );
 
+});
+//for ( var i = 0; i < reservations.length; i++) {
+console.log(reservations);
 
 });
