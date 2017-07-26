9eabad8f253658eb9bad9b6cef48c3e4cd4e6f28
Laura Senko <laurasenko@gmail.com>
2017-07-26 17:00:11 -0400

    javascript changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
     console.log( "ready!" );
+
+var reservations= [];
+var seatReservation= "";
+
+$(".seat").on('click', function() {
+	$("#form").show();
+	seatReservation = $(this).attr("id");
+  console.log(seatReservation);
+});
+
+$("#submit").on('click', function() {
+  var $name = $('#name').val();
+  var $phone = $('#phone').val();
+  var $email = $('#email').val();
+  var $movie = $('#movie').val();
+  var $time = $('#time').val();
+  console.log($name + $phone + $email + $movie + $time);
+});
+
+
 });
