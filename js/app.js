3ec5e3c449818ba0fdeab9fc163efa227b1025e6
Laura Senko <laurasenko@gmail.com>
2017-07-26 21:39:55 -0400

    css update

--- js/app.js
+++ js/app.js
@@ -1,33 +1,31 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function()
+
+console.log( "ready!" );
 
 var reservations= [];
 var $seatReservation= "";
 
-$(".seat").on('click', function() {
-	$("#form").show();
-	$seatReservation = $(this).attr("id");
-  reservations.push({seat: $seatReservation});
-  //console.log($seatReservation);
-});
+  $(".seat").on('click', function() {
+    $("#form").show();
+	   $seatReservation = $(this).attr("id");
+     reservations.push({seat: $seatReservation});
+  });
 
-$("#submit").on('click', function() {
-  var $name = $('#name').val();
-  var $phone = $('#phone').val();
-  var $email = $('#email').val();
-  var $movie = $('#movie').val();
-  var $time = $('#time').val();
-  //console.log($name + $phone + $email + $movie + $time);
-  reservations.push(
-    {name: $name},
-    {phone: $phone},
-    {email: $email},
-    {movie: $movie},
-    {time: $time}
+  $("#submit").on('click', function() {
+    var $name = $('#name').val();
+    var $phone = $('#phone').val();
+    var $email = $('#email').val();
+    var $movie = $('#movie').val();
+    var $time = $('#time').val();
+
+    reservations.push(
+      {name: $name},
+      {phone: $phone},
+      {email: $email},
+      {movie: $movie},
+      {time: $time}
   );
 
-});
-//for ( var i = 0; i < reservations.length; i++) {
-console.log(reservations);
 
+console.log(reservations);
 });
